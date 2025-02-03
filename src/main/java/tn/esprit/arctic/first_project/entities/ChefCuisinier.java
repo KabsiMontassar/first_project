b8eb2d2d1852e70f36cc8fe66dabf613e3382b82
KabsiMontassar <kebsimontassar@gmail.com>
2025-02-03 09:38:47 +0100

    Update ChefCuisinier.java

--- src/main/java/tn/esprit/arctic/first_project/entities/ChefCuisinier.java
+++ src/main/java/tn/esprit/arctic/first_project/entities/ChefCuisinier.java
@@ -1,9 +1,6 @@
 package tn.esprit.arctic.first_project.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 class ChefCuisinier {
